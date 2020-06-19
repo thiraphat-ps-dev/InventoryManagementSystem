64411b96182a8a85544665a6b083762da38750ee
gun <thiraphat.ps.dev@gmail.com>
2020-06-19 19:32:46 +0700

    add reset css and login page

---
+++ frontend/pages/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <main className="login-page">
+          <div className="login-container">
+              
+          </div>
+      </main>
+    );
+  }
+}
