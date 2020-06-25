dd6372751973255a6c5e49a5b4fcb393545be813
gun <thiraphat.ps.dev@gmail.com>
2020-06-25 13:41:10 +0700

    refactor login page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,17 @@
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
+import Router from 'next/router';
 
 class index extends Component {
+  componentDidMount() {
+    console.log(localStorage.getItem('access_token'));
+    const checkLogin = localStorage.getItem('access_token');
+    if (checkLogin === 'undefined' || checkLogin === null) {
+      Router.push({ pathname: '/login' });
+    }
+  }
+
   render() {
     console.log(this.props.countreducer);
     console.table(this.props.userdata.data);
